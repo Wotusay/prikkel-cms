62cac61ea01bf8a694d678a5810b5976b08926de
Wout Salembier <HA-SalemWou@electrolux.com>
2023-06-01 19:34:53 +0200

    chore: added new config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 import { visionTool } from '@sanity/vision';
-import { defineConfig } from 'sanity';
+import { defineConfig, isDev } from 'sanity';
 import { deskTool } from 'sanity/desk';
 
 import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash';
@@ -8,6 +8,8 @@ import { vercelDeployTool } from 'sanity-plugin-vercel-deploy';
 
 import { myStructure } from './helpers/deskStructure';
 import { schemaTypes } from './schemas';
+
+const devOnlyPlugins = [visionTool()];
 
 export default defineConfig({
     name: 'default',
@@ -20,9 +22,9 @@ export default defineConfig({
         deskTool({
             structure: myStructure,
         }),
-        visionTool(),
         unsplashImageAsset(),
         vercelDeployTool(),
+        ...(isDev ? devOnlyPlugins : []),
     ],
 
     schema: {
