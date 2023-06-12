fb12f8462178df29c9159e88d784fe1ef848af66
Wout Salembier <HA-SalemWou@electrolux.com>
2023-06-12 21:46:29 +0200

    chore: commented out un wanted items

--- helpers/deskStructure.ts
+++ helpers/deskStructure.ts
@@ -8,12 +8,17 @@ export const myStructure = (S: any) =>
             S.listItem()
                 .title('Website instellingen')
                 .child(S.document().schemaType('websiteSettings').documentId('websiteSettings')),
-            S.listItem()
-                .title('Formulier instellingen')
-                .child(S.document().schemaType('form').documentId('form')),
+            // S.listItem()
+            //     .title('Formulier instellingen')
+            //     .child(S.document().schemaType('form').documentId('form')),
 
             ...S.documentTypeListItems().filter(
                 (listItem: any) =>
-                    !['footer', 'websiteSettings', 'media.tag', 'form'].includes(listItem.getId()),
+                    ![
+                        'footer',
+                        'websiteSettings',
+                        'media.tag',
+                        // 'form'
+                    ].includes(listItem.getId()),
             ),
         ]);
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,10 @@
 import footer from './footer';
-import form from './form';
 import page from './page';
 import websiteSettings from './website-settings';
 
-export const schemaTypes = [page, footer, websiteSettings, form];
+export const schemaTypes = [
+    page,
+    footer,
+    websiteSettings,
+    // form
+];
